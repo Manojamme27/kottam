b41957ef4d000b8d65cc3993d56d25c63f5c785f
Manoj Amme <125076065+Manojamme27@users.noreply.github.com>
2025-11-26 12:05:18 +0530

    Update order.routes.js

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,7 +1,7 @@
 import { getOwnerStats } from "../controllers/orderStats.controller.js";
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
-import { acceptOrder, getCurrentOrder, getDeliveryBoyAssignment, getMyOrders, getTodayDeliveries, placeOrder, updateOrderStatus, verifyPayment, cancelOrder } from "../controllers/order.controllers.js"
+import { acceptOrder, getCurrentOrder, getDeliveryBoyAssignment, getMyOrders, getTodayDeliveries, placeOrder, updateOrderStatus, verifyPayment,getOrderById,  cancelOrder } from "../controllers/order.controllers.js"
 
 
 
@@ -17,9 +17,11 @@ orderRouter.post("/update-status/:orderId/:shopId", isAuth, updateOrderStatus)
 orderRouter.get('/accept-order/:assignmentId', isAuth, acceptOrder)
 orderRouter.get('/get-today-deliveries', isAuth, getTodayDeliveries)
 orderRouter.put("/cancel/:orderId", isAuth, cancelOrder)
+orderRouter.get("/get-order-by-id/:orderId", isAuth, getOrderById);
 
 
 
 
 
 export default orderRouter
+
