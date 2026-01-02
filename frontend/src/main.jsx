7df0c27963f1f758254c6cc1abc4da6ea2f184e9
Manoj Amme <125076065+Manojamme27@users.noreply.github.com>
2026-01-02 21:19:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+import "./index.css";
+import App from "./App.jsx";
+import { store } from "./redux/store.js";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-
-  </BrowserRouter>
-)
+  </StrictMode>
+);
