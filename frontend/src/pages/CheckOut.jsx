d5d907e1f14096dbc945cf94170d0119505a1496
Manoj Amme <125076065+Manojamme27@users.noreply.github.com>
2026-01-03 20:20:22 +0530

    Update CheckOut.jsx

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -123,6 +123,7 @@ function CheckOut() {
     try {
       const result = await axios.get(
         `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&format=json&apiKey=${apiKey}`
+        { withCredentials: false }
       );
       const place = result?.data?.results?.[0];
 
@@ -156,6 +157,7 @@ setAddressInput(fullAddress);
         `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(
           addressInput
         )}&apiKey=${apiKey}`
+        { withCredentials: false }
       );
       const { lat, lon } = result.data.features[0].properties;
       dispatch(setLocation({ lat, lon }));
@@ -450,6 +452,7 @@ setAddressInput(fullAddress);
 }
 
 export default CheckOut;
+
 
 
 
