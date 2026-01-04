6e6a072a381e253a683aeceb926dcd427069f152
Manoj Amme <125076065+Manojamme27@users.noreply.github.com>
2026-01-04 14:59:50 +0530

    Delete frontend/src/ProtectedRoute.jsx

--- frontend/src/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-const ProtectedRoute = ({ children }) => {
-  const { userData } = useSelector(state => state.user);
-  return userData ? children : <Navigate to="/signin" replace />;
-};
-
-export default ProtectedRoute;
