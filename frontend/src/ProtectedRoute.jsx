c83abf7f2f98c6e480cc0961a9c5d8f21487bf36
Manoj Amme <125076065+Manojamme27@users.noreply.github.com>
2026-01-04 14:58:27 +0530

    Update ProtectedRoute.jsx

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -2,17 +2,8 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ children }) => {
-  const { userData, authChecked } = useSelector(state => state.user);
-
-  if (!authChecked) {
-    return <div>Loading...</div>;
-  }
-
-  if (!userData) {
-    return <Navigate to="/signin" replace />;
-  }
-
-  return children;
+  const { userData } = useSelector(state => state.user);
+  return userData ? children : <Navigate to="/signin" replace />;
 };
 
 export default ProtectedRoute;
