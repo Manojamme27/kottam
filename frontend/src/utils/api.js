42d5c429cf155a93b5c44a84b410b73843770c95
Manoj Amme <125076065+Manojamme27@users.noreply.github.com>
2026-01-17 12:13:32 +0530

    Delete frontend/src/utils/api.js

--- frontend/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { serverUrl } from "../App";
-
-const api = axios.create({
-  baseURL: serverUrl,
-  withCredentials: true, // 🔥 ALWAYS SEND COOKIE
-});
-
-export default api;
