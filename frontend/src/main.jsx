b9963c2b8f7a077551b97602d1fcbbdff4c96c08
Manoj Amme <125076065+Manojamme27@users.noreply.github.com>
2026-01-20 17:37:50 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,12 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
+         <App />
       </Provider>
     </BrowserRouter>
   </StrictMode>
