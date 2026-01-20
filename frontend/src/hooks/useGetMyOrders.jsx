f2d7b8e3870953223da3091b3dcedf538b1d03ed
Manoj Amme <125076065+Manojamme27@users.noreply.github.com>
2026-01-20 14:30:30 +0530

    Update useGetMyOrders.jsx

--- frontend/src/hooks/useGetMyOrders.jsx
+++ frontend/src/hooks/useGetMyOrders.jsx
@@ -1,27 +1,29 @@
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
 import { setMyOrders } from "../redux/userSlice";
 import { serverUrl } from "../App";
 
 const useGetMyOrders = () => {
   const dispatch = useDispatch();
-  const { userData, authChecked } = useSelector(state => state.user);
 
   useEffect(() => {
-    if (!authChecked || !userData?._id) return;
+    // ✅ hydrate instantly
+    const cached = localStorage.getItem("myOrders");
+    if (cached) {
+      dispatch(setMyOrders(JSON.parse(cached)));
+    }
 
+    // ✅ background sync
     axios
       .get(`${serverUrl}/api/order/my-orders`, {
         withCredentials: true,
       })
-      .then(res => {
-        dispatch(setMyOrders(res.data || [])); // 🔥 DB IS TRUTH
+      .then((res) => {
+        dispatch(setMyOrders(res.data));
       })
-      .catch(() => {
-        dispatch(setMyOrders([]));
-      });
-  }, [authChecked, userData?._id, dispatch]);
+      .catch(() => {});
+  }, [dispatch]);
 };
 
 export default useGetMyOrders;
